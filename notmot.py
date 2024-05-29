907ab2eac707f88fe446be7d8c5b6550120edf24
mahdi-rfz <mahdifeyzolahy@gmail.com>
2024-05-29 14:32:50 +0330

    add some defs

---
+++ notmot.py
@@ -0,0 +1,37 @@
+import mysql.connector as sql
+from colorama import Fore
+import socket
+import argparse
+import requests
+import time 
+import os
+
+
+#Enter database information
+host = () # enter ip or etc
+user = () # enter database username
+passwd = () # enter database password
+database = () # enter databse name for creat connection 
+#---------------------------
+
+
+"""
+description :
+
+"""
+
+
+def clear_screen(): #you can use clear screen on windows/linux/mac
+    if os.name == "nt" :
+        os.system("cls")
+    else :
+        os.system("clear")
+        
+        
+def check_internet_connection(): #check internet connection with google domain
+    try:
+        socket.create_connection(("www.google.com", 80))
+        return True
+    except OSError:
+        return False
+    
