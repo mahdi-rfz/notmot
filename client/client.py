fbd899f6dad8ce505e2ee25bdcd4d59928500a02
mahdi-rfz <mahdifeyzolahy@gmail.com>
2024-09-28 02:08:15 +0330

    client app complete

--- client/client.py
+++ client/client.py
@@ -1,28 +1,10 @@
 import requests
 import configs
-
-url = "http://localhost:4444/notmot/listNote"
-
-data = {"username":"mahdi" , "password":"root", "data":"my name is bahar and welcome"}
-
-respone = requests.post(url , data=data)
-
-print((respone.text))
-
-
-
-
-
-
-
-
-
-
-
-
+import socket
+import os
 
 def addNote(data):
-    url = (f"http://{configs.domain}:4243/notmot/addNote")
+    url = (f"http://{configs.domain}:{configs.port}/notmot/addNote")
 
     data = {"username":configs.username ,
              "password":configs.password , 
@@ -30,16 +12,16 @@ def addNote(data):
     
     response = requests.post(url , data=data)
 
-    if str(respone.status_code) == "200":
-        return ["your Note added" , respone.text]
+    if response.ok == True:
+        return ["your Note added" , response.text]
     else : 
-        return "You have problem in add note"
+        return f"You have problem in add note , {response.status_code}"
     
 
 
 
 def delNote(id):
-    url = (f"http://{configs.domain}:4243/notmot/delNote")
+    url = (f"http://{configs.domain}:{configs.port}/notmot/delNote")
 
     data = {"username":configs.username ,
              "password":configs.password , 
@@ -47,24 +29,94 @@ def delNote(id):
     
     response = requests.post(url , data=data)
 
-    if str(respone.status_code) == "200":
-        return ["your Note deleted" , respone.text]
+    if response.ok == True:
+        return ["your Note deleted" , response.text]
     else : 
-        return "You have problem in del note"
+        return f"You have problem in add note , {response.status_code}"
     
 
 
 
 
-def listNote(id):
-    url = (f"http://{configs.domain}:4243/notmot/listNote")
+def listNote():
+    url = (f"http://{configs.domain}:{configs.port}/notmot/listNote")
 
     data = {"username":configs.username ,
              "password":configs.password}
     
     response = requests.post(url , data=data)
 
-    if str(respone.status_code) == "200":
-        return [respone.text]
+    if response.ok == True:
+        return [response.text]
     else : 
-        return "You have problem in receive notes"
+        return f"You have problem in add note , {response.status_code}"
+    
+
+
+
+def textArt():
+    return """ █▄ █ ▄▀▄ ▀█▀ █▄ ▄█ ▄▀▄ ▀█▀
+ █ ▀█ ▀▄▀  █  █ ▀ █ ▀▄▀  █ 
+"""
+
+
+def check_internet_connection():
+        try:
+            socket.create_connection(("www.google.com", 80))
+            return True
+        except OSError:
+            return False
+        
+
+
+
+
+
+def main():
+    print("[1] - Add note")
+    print("[2] - Del note")
+    print("[3] - Show nots")
+    print("[4] - EXIT")
+    print()
+
+    choose = input(">>>")
+
+    if choose == "1" or choose == "add":
+        note = input("Enter your note >>>")
+        receive = addNote(note)
+        print()
+        print(f"{list(receive)[0]} with id : {eval(list(receive)[1])["id"]}")
+
+        print()
+        main()
+    elif choose == "2" or choose == "del":
+        note = input("Enter note id >>>")
+        receive = delNote(note)
+        print()
+        print(f"{list(receive)[0]} with id : {eval(list(receive)[1])["id"]}")
+
+        print()
+        main()
+    elif choose == "3" or choose == "show":
+        receive = listNote()
+
+        for note in eval((list(receive))[0]):
+            print(f"ID : {note["id"]} , Note : {note["note"]}")
+
+        print()
+        main()
+    elif choose == "clear" :
+        os.system("clear")
+        print(textArt())
+        main()
+            
+
+
+if check_internet_connection() == True:
+    os.system("clear")
+    print(textArt())
+    main()
+else : 
+    os.system("clear")
+    print(textArt())
+    print("You have problem in your internet connection")
--- client/configs.py
+++ client/configs.py
@@ -1,4 +1,5 @@
-domain = ("localhost")
+domain = ("46.100.60.199")
+port = ("4444")
 
 username = "mahdi"
 password = "root"
